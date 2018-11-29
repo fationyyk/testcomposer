102f98ccb3b4e80538cc52c5ab941a6ed04c27fc
fationyyk <fation@126.com>
2018-11-30 00:09:42 +0800

    Update Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -10,7 +10,7 @@ class Plugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
-        echo '111111';
+        
         $installer = new PluginInstaller($io, $composer);
       
         $composer->getInstallationManager()->addInstaller($installer);

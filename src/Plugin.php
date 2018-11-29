5f21daac76aaaa33219617748fbfc13f183fcb97
fationyyk <fation@126.com>
2018-11-29 23:33:48 +0800

    Update Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -10,8 +10,9 @@ class Plugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
-       
+        echo '111111';
         $installer = new PluginInstaller($io, $composer);
+        var_dump($installer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
 }

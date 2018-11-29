4abbd9b709175f9508afa0f863aef8a2856e0393
fationyyk <fation@126.com>
2018-11-30 00:07:54 +0800

    Update PluginInstaller.php

--- src/PluginInstaller.php
+++ src/PluginInstaller.php
@@ -10,7 +10,7 @@ class PluginInstaller extends LibraryInstaller
     /**
      * {@inheritDoc}
      */
-    public function getPackageBasePath(PackageInterface $package)
+    public function getInstallPath(PackageInterface $package)
     {
         $prefix = substr($package->getPrettyName(), 0, 23);
    
